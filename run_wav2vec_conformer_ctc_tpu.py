24b9950b39d42307d86190c5fb40b0c051205bd8
天逸 <htxu91@gmail.com>
2021-04-19 23:01:32 +0800

    conformer-pinyin-ctc

--- run_wav2vec_conformer_ctc_tpu.py
+++ run_wav2vec_conformer_ctc_tpu.py
@@ -432,7 +432,7 @@ def input_fn_builder(input_file,
     }
     batch_size = params["batch_size"]
     def _decode_record(record, name_to_features):
-      """Decodes a record to a TensorFlow example."""
+      """Decodes a  record to a TensorFlow example."""
       example = tf.parse_single_example(record, name_to_features)
 
       # tf.Example only supports tf.int64, but the TPU only supports tf.int32.
