c5c00475b6bd0b534ecf77e0024d60c0e1bd77e6
天逸 <htxu91@gmail.com>
2021-04-20 17:41:12 +0800

    conformer-pinyin-ctc

--- run_wav2vec_conformer_ctc_tpu_base_pinyin.sh
+++ run_wav2vec_conformer_ctc_tpu_base_pinyin.sh
@@ -7,7 +7,7 @@ nohup python ./run_wav2vec_conformer_ctc_tpu.py \
 	--max_seq_length 512 \
 	--do_train True \
 	--train_batch_size 128 \
-	--learning_rate 3e-4 \
+	--learning_rate 1e-4 \
 	--num_train_steps 200000 \
 	--num_warmup_steps 20000 \
 	--init_checkpoint chinese_asr_v1/conformer_pretrain_v1_linear/model.ckpt-200000 \
