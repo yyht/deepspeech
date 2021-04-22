5b77b6d37ad52d85875430cb505d29298add10e6
天逸 <htxu91@gmail.com>
2021-04-22 09:26:17 +0800

    add enc-dec

--- run_wav2vec_conformer_tpu.py
+++ run_wav2vec_conformer_tpu.py
@@ -532,12 +532,12 @@ def input_fn_builder(input_file,
       feature_shape = shape_list(clean_feature)
       # [T, V, 1]
       output_examples['feature_seq_length'] = tf.cast(feature_shape[0], dtype=tf.int32)
-      [unique_labels, 
-      unique_indices] = ctc_ops.ctc_unique_labels(
-              tf.cast(example['transcript_id'], dtype=tf.int32)
-              )
-      output_examples['unique_labels'] = tf.cast(unique_labels, dtype=tf.int32)
-      output_examples['unique_indices'] = tf.cast(unique_indices, dtype=tf.int32)
+      # [unique_labels, 
+      # unique_indices] = ctc_ops.ctc_unique_labels(
+      #         tf.cast(example['transcript_id'], dtype=tf.int32)
+      #         )
+      # output_examples['unique_labels'] = tf.cast(unique_labels, dtype=tf.int32)
+      # output_examples['unique_indices'] = tf.cast(unique_indices, dtype=tf.int32)
       
       reduced_length = audio_utils.get_reduced_length(feature_shape[0], audio_featurizer.get_reduced_factor())
 
@@ -594,8 +594,8 @@ def input_fn_builder(input_file,
                 "transcript_id":tf.TensorShape([transcript_seq_length]),
                 "gender_id":tf.TensorShape([]),
                 "dialect_id":tf.TensorShape([]),
-                "unique_labels":tf.TensorShape([transcript_seq_length]),
-                "unique_indices":tf.TensorShape([transcript_seq_length]),
+                # "unique_labels":tf.TensorShape([transcript_seq_length]),
+                # "unique_indices":tf.TensorShape([transcript_seq_length]),
                 "feature_seq_length":tf.TensorShape([]),
                 "masked_positions":tf.TensorShape([num_predict]),
                 "masked_weights":tf.TensorShape([num_predict]),
@@ -612,8 +612,8 @@ def input_fn_builder(input_file,
                 "transcript_id":0,
                 "gender_id":0,
                 "dialect_id":0,
-                "unique_labels":0,
-                "unique_indices":0,
+                # "unique_labels":0,
+                # "unique_indices":0,
                 "feature_seq_length":0,
                 "masked_positions":0,
                 "masked_weights":0.0,
